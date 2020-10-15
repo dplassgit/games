8b52e7de937bfaa3fe44d6a8d035b7674447db1d
David Plass <dplassgit@users.noreply.github.com>
2020-10-14 22:58:56 -0400

    Update the sin & cos tables to have 78 entries instead of 40. "Sweep" the center of the radar. Set up the "enemy [direction]" and "score" labels

--- Baseball/scroll.asm
+++ Baseball/scroll.asm
@@ -4,88 +4,109 @@
 ; Copies from /1 to /2 until a 0 is found at /1
 ; Destroys a, y
 defm           COPY0
-               LDY #0      
+               LDY #0
 @nextch        LDA /1,y
                beq @exitmac
-               sta /2,y    
+               sta /2,y
                iny
-               jmp @nextch 
+               jmp @nextch
 @exitmac
                endm
 
 ; Copies from /1 to /2 until a 0 is found at /1
 ; If there's a space in the source, does not overwrite output
 defm           COPY0_NOSPACE
-               LDY #0      
+               LDY #0
 @nextch        LDA /1,y
                beq @exitmac
-               cmp #32     
+               cmp #32
                beq @nexty
-               sta /2,y    
+               sta /2,y
 @nexty         iny
-               jmp @nextch 
+               jmp @nextch
 @exitmac
                endm
 
 ; Copies up to 40 characters from /1 (offset in y) to /2
 ; Destroys a, x, y
 defm           COPY40
-               LDX #0      
+               LDX #0
 @nextch        LDA /1,y
                beq @start_over
-               sta /2,x    
+               sta /2,x
                iny
                inx
-               cpx #40     
+               cpx #40
                beq @exitmac
-               jmp @nextch 
+               jmp @nextch
 
 @start_over    ldy #0  ; start from the beginning of the string
-               jmp @nextch 
+               jmp @nextch
 
-@exitmac        
+@exitmac
                endm
 
 
-start          jmp polar_test    
-               
-               COPY0 enemy,enemy_loc
-               COPY0 radar1,radar1_loc
-               COPY0 radar2,radar2_loc
-               COPY0 radar3,radar3_loc
-               COPY0 radar4,radar4_loc
+start
+               COPY0 enemy,enemy_1_org
+               COPY0 enemy_left,enemy_2_org
+               COPY0 score,score_org
+               COPY0 radar1,radar1_org
+               COPY0 radar2,radar2_org
+               COPY0 radar3,radar3_org
+               COPY0 radar4,radar4_org
 
-               ldx #40     
-               lda #99     
-horizon_loop   STA hor_loc,x
+               ldx #40
+               lda #99
+horizon_loop   STA horizon_org,x
                dex
                bne horizon_loop
 
-               COPY0 bottom_ret1,bottom_ret1_loc
-               COPY0 bottom_ret2,bottom_ret2_loc
-               COPY0 bottom_ret3,bottom_ret3_loc
+               COPY0 bottom_ret1,bottom_ret1_org
+               COPY0 bottom_ret2,bottom_ret2_org
+               COPY0 bottom_ret3,bottom_ret3_org
 
-reset_bg       LDY #0
-               sty angle   
+reset_bg
+              LDY #0
+              sty angle
 
-bg_loop        LDY angle
-               COPY40 bg0,bg_loc
-               ldy angle   
-               COPY40 bg1,bg_loc+40
-               ldy angle   
-               COPY40 bg2,bg_loc+80
-               ldy angle   
-               COPY40 bg3,bg_loc+120
-               ldy angle   
-               COPY40 bg4,bg_loc+160
-               ldy angle   
-               COPY40 bg5,bg_loc+200
+bg_loop
+               ; update the radar sweep
+               inc sweep
+               lda sweep
+               and #96
+               clc
+               ror
+               clc
+               ror
+               clc
+               ror
+               clc
+               ror
+               clc
+               ror
+               tax
+               lda radar_sweep,x
+               sta radar_sweep_org
 
-               lda showtop     
+               LDY angle
+               COPY40 bg0,bg_org
+               ldy angle
+               COPY40 bg1,bg_org+40
+               ldy angle
+               COPY40 bg2,bg_org+80
+               ldy angle
+               COPY40 bg3,bg_org+120
+               ldy angle
+               COPY40 bg4,bg_org+160
+               ldy angle
+               COPY40 bg5,bg_org+200
+
+               lda showtop
                beq waiting
-               COPY0_NOSPACE top_ret1,top_ret1_loc
-               COPY0_NOSPACE top_ret2,top_ret2_loc
-               COPY0_NOSPACE top_ret3,top_ret3_loc
+               COPY0_NOSPACE top_ret1,top_ret1_org
+               COPY0_NOSPACE top_ret2,top_ret2_org
+               COPY0_NOSPACE top_ret3,top_ret3_org
 
 waiting        LDA 151
                cmp #$ff
@@ -99,96 +120,117 @@ akey           CMP #48     ; 0 = reset
 
 maybe_left     CMP #74     ; j
                bne maybe_right
-               dec angle   
-               lda angle   
-               cmp #$ff    
+               dec angle
+               lda angle
+               cmp #$ff
                beq goto_end
-               jmp bg_loop 
+               jmp bg_loop
 
 goto_end       LDY #BG_LENGTH
-               sty angle   
-               jmp bg_loop 
+               sty angle
+               jmp bg_loop
 
 maybe_right    CMP #76     ; l
-               bne maybe_quit 
-               inc angle   
-               lda angle   
-               cmp #BG_LENGTH+1     
-               beq jmp_reset_bg    
-               jmp bg_loop 
+               bne maybe_quit
+               inc angle
+               lda angle
+               cmp #BG_LENGTH+1
+               beq jmp_reset_bg
+               jmp bg_loop
 
 maybe_quit     CMP #81  ;q
-               beq quit   
-               
+               beq quit
+
 ;maybe_toggle   CMP #84  ;t
 ;               bne jmp_bg_loop
 ;               sec
 ;               lda #1
-;               sbc showtop 
+;               sbc showtop
 ;               sta showtop
 
 jmp_bg_loop    jmp bg_loop
 jmp_reset_bg   JMP reset_bg
 
-quit                   
+quit
 
-polar_test     LDA #40
-               sta angle 
-               
-polar_loop     LDX angle
+polar_test     LDA #78
+               sta angle2
+
+polar_loop     ldx angle2
                dex
-               ldy #15 ; radius
+               ldy #5 ; radius
                ; theta in x, radius in y
                jsr from_polar
+               ; add 32768+2*40+19 to polar_result
+               ; = 32867, in hex, 8063
+               clc
+               lda #$64
+               adc polar_result
+               sta polar_result
+               lda #$80
+               adc polar_result+1
+               sta polar_result+1
                ldy #0
-               lda #"."   
+               lda #"."
                sta (polar_result),y
-               dec angle
+               dec angle2
                bne polar_loop
 
-               lda #"*"     
-               sta $81f2
-
+               lda #"*"
+               sta $8063
                rts
 
 incasm "fixedpoint.asm"
 incasm "trig.asm"
 
 angle          byte 0
+angle2 byte 0
 showtop        byte 1
 
-hor_loc        = 32767+40*14
+horizon_org        = 32767+40*14
 
-radar1_loc     = $8011
-radar2_loc     = radar1_loc+41
-radar3_loc     = radar1_loc+80
-radar4_loc     = radar1_loc+162
+radar1_org     = $8011
+radar2_org     = radar1_org+41
+radar3_org     = radar1_org+80
+radar4_org     = radar1_org+162
 radar1         byte $4d,$20,$5d,$20,$4e,0
 radar2         byte $4d,$20,$4e,0
-radar3         byte $40,$20,42,$20,$40,0
+radar3         byte $40,$20,67,$20,$40,0
 radar4         byte $5d, 0
 
+radar_sweep_org  = radar3_org+2
+sweep          byte 0
+radar_sweep    byte 67,78,$5d,77
+radar_sweep_len = 4
+
 ; bottom reticle
-bottom_ret1_loc = 32768+16*40+17
-bottom_ret2_loc = bottom_ret1_loc+40
-bottom_ret3_loc = bottom_ret2_loc+42
+bottom_ret1_org = 32768+16*40+17
+bottom_ret2_org = bottom_ret1_org+40
+bottom_ret3_org = bottom_ret2_org+42
 bottom_ret1    byte 101,$20,$20,$20,103,0
 bottom_ret2    byte 99,99,$5D,99,99,0
 bottom_ret3    byte $5d,0
 
 ; top reticle
-top_ret1_loc   = 32768+9*40+19
-top_ret2_loc   = top_ret1_loc+40-2
-top_ret3_loc   = top_ret2_loc+39
+top_ret1_org   = 32768+9*40+19
+top_ret2_org   = top_ret1_org+40-2
+top_ret3_org   = top_ret2_org+39
 top_ret1       byte $5D,0
 top_ret2       byte 100,100,93,100,100,0
 top_ret3       byte 103,$20,$20,$20,$20,$20,101,0
 
-enemy_loc      = 32768
+enemy_1_org    = 32768
 enemy          null 'enemy in range'
+enemy_2_org    = 32768+80
+enemy_right    null 'enemy to right'
+enemy_left     null 'enemy to left'
+enemy_rear     null 'enemy to rear'
+
+score_org      = 32768+40+25
+score          null 'score'
 
 ; backgrounds
-bg_loc          = 32768+8*40
+bg_org          = 32768+8*40
 BG_LENGTH       = 78
 bgn            null '123456789012345678901234567890123456789012345678901234567890123456789012345678'
 bg0            null 'UI               .                                                     .      '
--- Baseball/trig.asm
+++ Baseball/trig.asm
@@ -9,70 +9,61 @@
 
 ; inputs: theta in x, radius in y
 
-from_polar     stx theta
+from_polar     STX theta
                sty radius
                lda costab,x
                tax
                jsr mpy
-               ; throws out the low byte, which probably causes rounding errors
+                           ; throws out the low byte, which probably causes rounding errors
                lda result+1
-               ; this is scaled 50%, which is wrong
+                           ; this is scaled 50%, which is wrong
                sta polarx
-               
+
                ldx theta
                lda sintab,x
                tax
                ldy radius
-               jsr mpy     
+               jsr mpy
                lda result+1
-               ; this is scaled 50%, which is wrong
+                           ; this is scaled 50%, which is wrong
                sta polary
 
                tay
                ldx #40
-               jsr mpy  ; multiply polary by 40
+               jsr mpy     ; multiply polary by 40
 
-               ; copy 2 byte signed result to polar_result
+                           ; copy 2 byte signed result to polar_result
                lda result+1
                sta polar_result+1
                lda result
                sta polar_result
 
-               ; add (signed) polar x to polar result -- this part is
-               ; annoying since adc doesn't do signed addition...
+                           ; add (signed) polar x to polar result -- this part is
+                           ; annoying since adc doesn't do signed addition...
                lda polarx
                beq add32768
-               bpl pos     
-               ; negate a:
-               eor #$ff    
+               bpl pos
+                           ; negate a:
+               eor #$ff
                clc
-               adc #$01    
-               ; stash updated polarx
-               sta polarx  
-               ; subtract polarx from polar_result
+               adc #$01
+                           ; stash updated polarx
+               sta polarx
+                           ; subtract polarx from polar_result
                lda polar_result
                sec
-               sbc polarx  
+               sbc polarx
                sta polar_result
                bcs add32768
                dec polar_result+1
                jmp add32768
-               
-pos            adc polar_result
+
+pos            ADC polar_result
                sta polar_result
                bcc add32768
                inc polar_result+1
 
-add32768       ; add 32768+12*40+20 to polar_result
-               ; = 33268, in hex, 81F4
-               clc
-               lda #$f3    
-               adc polar_result
-               sta polar_result
-               lda #$81    
-               adc polar_result+1
-               sta polar_result+1
-               rts
+add32768       rts
 
 
 ; there must be a way to reduce these...
@@ -82,6 +73,9 @@ polarx         byte 0
 polary         byte 0
 ; zero page
 polar_result   = $fd
-               
-sintab         byte $0A,$1E,$31,$42,$52,$61,$6C,$75,$7B,$7F,$7F,$7B,$75,$6C,$61,$52,$42,$31,$1E,$0A,$F6,$E2,$CF,$BE,$AE,$9F,$94,$8B,$85,$81,$81,$85,$8B,$94,$9F,$AE,$BE,$CF,$E2,$F6,$0A
-costab         byte $7F,$7B,$75,$6C,$61,$52,$42,$31,$1E,$0A,$F6,$E2,$CF,$BE,$AE,$9F,$94,$8B,$85,$81,$81,$85,$8B,$94,$9F,$AE,$BE,$CF,$E2,$F6,$0A,$1E,$31,$42,$52,$61,$6C,$75,$7B,$7F,$7f
+
+sintab40       byte $0A,$1E,$31,$42,$52,$61,$6C,$75,$7B,$7F,$7F,$7B,$75,$6C,$61,$52,$42,$31,$1E,$0A,$F6,$E2,$CF,$BE,$AE,$9F,$94,$8B,$85,$81,$81,$85,$8B,$94,$9F,$AE,$BE,$CF,$E2,$F6,$0A
+costab40       byte $7F,$7B,$75,$6C,$61,$52,$42,$31,$1E,$0A,$F6,$E2,$CF,$BE,$AE,$9F,$94,$8B,$85,$81,$81,$85,$8B,$94,$9F,$AE,$BE,$CF,$E2,$F6,$0A,$1E,$31,$42,$52,$61,$6C,$75,$7B,$7F,$7f
+
+sintab         byte $04, $0e, $18, $22, $2c, $35, $3e, $47, $4f, $57, $5e, $65, $6a, $70, $74, $78, $7b, $7d, $7e, $7e, $7e, $7d, $7b, $78, $75, $70, $6b, $65, $5f, $58, $50, $48, $40, $37, $2d, $23, $1a, $0f, $05, $fc, $f2, $e8, $de, $d4, $cb, $c2, $b9, $b1, $a9, $a2, $9b, $96, $90, $8c, $88, $85, $83, $82, $82, $82, $83, $85, $88, $8b, $90, $95, $9b, $a1, $a8, $b0, $b8, $c0, $c9, $d3, $dd, $e6, $f1, $fb
+costab         byte $7e, $7e, $7c, $7a, $76, $73, $6e, $68, $62, $5c, $54, $4c, $44, $3b, $32, $28, $1f, $15, $0a, $01, $f7, $ed, $e3, $d9, $cf, $c6, $bd, $b5, $ad, $a5, $9f, $98, $93, $8e, $8a, $87, $84, $83, $82, $82, $82, $84, $86, $8a, $8d, $92, $98, $9e, $a4, $ac, $b4, $bc, $c5, $ce, $d8, $e1, $eb, $f6, $01, $09, $13, $1d, $27, $31, $3a, $43, $4b, $53, $5b, $61, $68, $6d, $72, $76, $79, $7c, $7d, $7e
