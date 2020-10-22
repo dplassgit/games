88571b7a452fcd1298555e4db13668e9203def64
David Plass <dplassgit@users.noreply.github.com>
2020-10-21 22:27:31 -0400

    Updated to 160 steps

--- TankZone/singen.py
+++ TankZone/singen.py
@@ -3,16 +3,18 @@ import math
 def tohex(val, nbits=16):
   return hex((val + (1 << nbits)) % (1 << nbits))
 
+num=160
+
 print "sintab byte "
-for i in range(0,79):
-  theta = (360.0/78.0)*i+2
+for i in range(0,num-1):
+  theta = (360.0/num)*i+1
   val = tohex(int(127*math.sin(math.radians(theta))))
   # print "%d %d $%s," % (i, theta,val[-2:]),
   print "$%s," % val[-2:],
 print
 print "costab byte "
-for i in range(0,79):
-  theta = (360.0/78.0)*i+2
+for i in range(0,num-1):
+  theta = (360.0/num)*i+1
   val = tohex(int(127*math.cos(math.radians(theta))))
   # print "%d %d $%s," % (i, theta,val[-2:]),
   print "$%s," % val[-2:],
